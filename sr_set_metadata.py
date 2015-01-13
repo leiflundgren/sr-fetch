52325f327d7f9d265e9706c6d41cc2d0af26039f
Leif Lundgren <leiflundgren@leiflundgren.com>
2015-01-13 23:31:36 +0100

    fresh copy from svn, ignoring all history. Consider this a gresh start.

---
+++ sr_set_metadata.py
@@ -0,0 +1,203 @@
+#!/usr/bin/python
+
+import sys
+import argparse
+import subprocess
+import re
+import os
+from datetime import timedelta
+import time
+import unittest
+import glob
+import dateutil.parser
+import common
+
+def isEmpty(value):
+    return value is None or len(value) == 0
+
+
+class SrSetMetadata:
+    """Set metadata of m4a from filename"""
+
+    def __init__(self, filename, overwrite, tracelevel, AtomicParsley):
+        self.filename = filename
+        self.overwrite = overwrite
+        self.tracelevel = tracelevel
+        common.tracelevel = tracelevel
+        if AtomicParsley is None:
+            self.AtomicParsley = 'AtomicParsley'
+        else:
+            self.AtomicParsley = AtomicParsley
+
+        self.trace(4, 'SrSetMetadata filename=%s  overwrite=%s  tracelevel=%d  AtomicParslay="%s"' % (self.filename, self.overwrite, self.tracelevel, self.AtomicParsley) )
+        
+    def trace(self, level, msg):
+        common.trace(level, msg)
+
+    def parse_filename(self):
+        self.trace(6, 'Parsing ' + self.filename)
+
+        m = re.match('^(.*) (\d\d\d\d\-\d\d?\-\d\d?)([a-c]?) +([^ ].*)\.m4a$', self.filename)
+        
+        if m is None:
+            m = re.match('^(.*) (\d\d\d\d\d\d)([a-c]?) +([^ ].*)\.m4a$', self.filename)
+
+        if m is None:
+            m = re.match('^(.+) (\d\d\d\d\d\d)([a-c]?)( +[^ ].*)?\.m4a$', self.filename)
+
+        if m is None:
+            raise ValueError('Bad format of file "' + self.filename + '" doesn\'t match regex')
+
+        # self.trace(7, 'match-parts ' + str(list(m.group[1:])))
+
+        self.progname = m.group(1).strip()
+        if self.progname == 'Lexsommar':
+            self.artist = 'Lex'
+        elif self.progname == 'Lordagsmorgon':
+            self.artist = 'Eric Schult'
+        else:
+            self.artist = self.progname
+
+            
+            
+        self.date = m.group(2)
+        self.part = m.group(3)
+        self.desc = m.group(4).strip()
+
+       
+
+    def atomic_parsly_commandline(self):
+        desc = self.desc
+        if desc.endswith('..') and not desc.endswith('...'):
+            desc += '.'
+        date = self.date
+        
+        if self.progname == 'Lexsommar':
+            timeofday = '17:00:00' if self.part != 'b' else '18:10:00'
+            date = '%sT%s' % (date, timeofday)
+
+        return [ self.AtomicParsley, self.filename, '--artist', self.artist, '--album', self.progname, '--title', self.date + self.part + ' ' + desc, '--year', date ]
+       
+    def rename_parsley_output(self):
+        self.parse_filename()
+        temp_pattern = '%s %s%s %s-temp-*.m4a' % (self.progname, self.date, self.part, self.desc)
+        temp_files = glob.glob(temp_pattern)
+
+        if len(temp_files) == 0:
+            self.trace(6, 'No tempfiles found for ' + self.filename)
+            return None
+        if len(temp_files) > 1:
+            self.trace(3, 'Found %d found for %s. Using first %s' % (len(temp_files), self.filename, temp_files[0]))
+
+        self.trace(4, 'Deleting "%s" and replacing with parsley output "%s"' % (self.filename, temp_files[0]))
+        os.remove(self.filename)
+        os.rename(temp_files[0], self.filename)
+        
+    def touch_creation_time(self):
+        self.parse_filename()
+        timeofday = '17:00:00' if self.part != 'b' else '18:10:00'
+
+        tstr = '%sT%s+0200' % (self.date, timeofday)
+
+        dt = dateutil.parser.parse(tstr)
+        new_mtime = time.mktime(dt.timetuple())
+        self.trace(7, 'Setting modification time to ' + SrSetMetadata.format_datetime(dt) + ' (' + str(new_mtime) + ' seconds since 1970)')
+
+
+        st = os.stat(self.filename)
+        atime = st.st_atime #access time
+        mtime = st.st_mtime #modification time
+
+
+        #modify the file timestamp
+        os.utime(self.filename,(atime,new_mtime))
+
+    @staticmethod
+    def format_datetime(dt):
+        return dt.strftime('%Y-%m-%dT%H:%M:%S%z')
+
+    def main(self):
+        self.parse_filename()
+        cmd = self.atomic_parsly_commandline()
+        self.trace(5, 'Calling ' + str(cmd))
+        subprocess.call(cmd)
+
+        if self.overwrite:
+            self.rename_parsley_output()
+        self.touch_creation_time()
+
+
+class TestSrSetMetadata(unittest.TestCase):
+    def setUp(self):
+        return super(TestSrSetMetadata, self).setUp()
+
+    def test_misc(self):
+
+        dt0 = timedelta()
+        dt1 = timedelta(minutes=1)
+        dt2 = timedelta(minutes=2)
+
+
+        self.assertEqual(dt1, dt0+dt1)
+        self.assertEqual(timedelta(minutes=3), dt1+dt2)
+
+    def test_parse_filename(self):
+        testcases = ( 
+            # orig, name, data, a/b-field, desc
+           (    'Lexsommar 2000-00-00 Not really a real episode.m4a', 
+                ['AtomicParsley/AtomicParsley', 'Lexsommar 2000-00-00 Not really a real episode.m4a', '--artist', 'Lex', '--album', 'Lexsommar', '--title', '2000-00-00 Not really a real episode', '--year', '2000-00-00T17:00:00Z'],
+                ('Lexsommar', '2000-00-00', '', 'Not really a real episode') 
+           ),
+           (    'Lexsommar 2000-00-00a Not really a real episode.m4a', 
+                ['AtomicParsley/AtomicParsley', 'Lexsommar 2000-00-00a Not really a real episode.m4a', '--artist', 'Lex', '--album', 'Lexsommar', '--title', '2000-00-00a Not really a real episode', '--year', '2000-00-00T17:00:00Z'],
+                ('Lexsommar', '2000-00-00', 'a', 'Not really a real episode') 
+           ),
+           (    'Lexsommar 2000-00-00a Not really a real episode.m4a', 
+                ['AtomicParsley/AtomicParsley', 'Lexsommar 2000-00-00a Not really a real episode.m4a', '--artist', 'Lex', '--album', 'Lexsommar', '--title', '2000-00-00a Not really a real episode', '--year', '2000-00-00T17:00:00Z'],
+                ('Lexsommar', '2000-00-00', 'a', 'Not really a real episode') 
+           ),
+        ) 
+
+        for t in testcases:
+            expected = t[2]
+
+            ssm = SrSetMetadata(t[0], True, 8, 'AtomicParsley/AtomicParsley')
+            ssm.parse_filename()
+            self.assertEqual(expected[0], ssm.progname)
+            self.assertEqual('Lex', ssm.artist)
+            self.assertEqual(expected[1], ssm.date)
+            self.assertEqual(expected[2], ssm.part)
+            self.assertEqual(expected[3], ssm.desc)
+
+            expected = t[1]
+            cmd = ssm.atomic_parsly_commandline()
+            self.assertEquals(expected, cmd)
+
+
+if __name__ == '__main__':
+
+    print 'argv: ' + str(sys.argv[1:])
+    #sys.exit(1)
+
+    for a in sys.argv:
+        if a == '--unittest':
+            sys.argv = ['-v']
+            sys.exit(unittest.main())
+
+    parser = argparse.ArgumentParser(description='My favorite argparser.')
+    parser.add_argument('-y', '--overwrite', help='Overwrite exisitng files', action='store_true', default=False, required=False, dest='overwrite')
+    parser.add_argument('-l', '--tracelevel', help='Verbosity level 1 is important like error, 9 is unneeded debuginfo', default=4, type=int)
+    parser.add_argument('--atomic-parsley', help='Location of atomic-parsley', dest='AtomicParsley', default='AtomicParsley')
+    parser.add_argument('filenames', metavar='filename.m4a', help='filename to remove kulturnytt from', nargs='+')
+
+                    
+    args = parser.parse_args()
+
+    for f in args.filenames:
+        SrSetMetadata(f, args.overwrite, args.tracelevel, args.AtomicParsley).main()
+       
+
+
+
+
+
