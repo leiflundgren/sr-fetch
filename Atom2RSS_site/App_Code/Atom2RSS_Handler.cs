560f33212b5fdd68f0d862008c492d7019f93ebd
Leif Lundgren <leiflundgren@leiflundgren.com>
2015-05-14 15:35:58 +0200

    Tests with a custom http handler

---
+++ Atom2RSS_site/App_Code/Atom2RSS_Handler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Atom2RSS
+{
+
+    /// <summary>
+    /// Summary description for Atom2RSS_Handler
+    /// </summary>
+    public class Handler : IHttpHandler
+    {
+
+        public Handler()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        // Override the ProcessRequest method. 
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("<H1>This is an HttpHandler Test.</H1>");
+            context.Response.Write("<p>Your Browser:</p>");
+            context.Response.Write("Type: " + context.Request.Browser.Type + "<br>");
+            context.Response.Write("Version: " + context.Request.Browser.Version);
+        }
+
+        // Override the IsReusable property. 
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+    }
+}
