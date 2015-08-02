ee4f41f95ce06c9cb636ea8f6e89998c58c7510f
Leif Lundgren <leiflundgren@leiflundgren.com>
2015-08-02 18:46:09 +0200

    sr_feed_app can handle html as source

--- sr_feed_app.py
+++ sr_feed_app.py
@@ -18,10 +18,20 @@ class SrFeedApp(AppBase):
             return ['parameter programid must be numbers!']
 
         proxy_data = self.qs_get('proxy_data', 'False').lower() == 'true'
-        format = self.qs_get('format') 
+        format = self.qs_get('format', 'rss') 
 
-        self.log(4, 'Attempt to find prog=' + str(programid)  + ', proxy_data = ' + str(proxy_data))
-        feeder = sr_feed.SrFeed(self.base_url, 'http://api.sr.se/api/rss/program/' + str(programid), self.tracelevel, format, proxy_data)
+        source = self.qs_get('source', 'feed')
+
+        if source == 'feed':
+            prog_url = 'http://api.sr.se/api/rss/program/' + str(programid)
+        elif source == 'html':
+            prog_url = 'http://sverigesradio.se/sida/avsnitt?programid=' + str(programid)
+        else:
+            self.start_response("500", [("Content-Type", "text/plain")])
+            return ['unsupported source. Must be feed/html!']
+
+        self.log(4, 'Attempt to find prog=' + str(programid)  + ', proxy_data = ' + str(proxy_data) + ' from ' + prog_url)
+        feeder = sr_feed.SrFeed(self.base_url, prog_url, str(programid), self.tracelevel, format, proxy_data)
         feed_data = feeder.get_feed()
         headers = [     
             ("Content-Type", feeder.content_type),
