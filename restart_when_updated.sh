ad32e51bed995b16bdc0afe96d9874fd39193616
Leif Lundgren <leiflundgren@leiflundgren.com>
2016-05-14 19:17:59 +0000

    refactored

--- restart_when_updated.sh
+++ restart_when_updated.sh
@@ -3,11 +3,19 @@
 echo "leifdev restarter started"
 touch /etc/uwsgi/apps-available/leifdev.leiflundgren.com.ini
 
-while true; do
+has_changed() {
+    r=1
     for f in `find /home/leif/src/py_web -name '*.py' -cnewer /etc/uwsgi/apps-available/leifdev.leiflundgren.com.ini`; do 
-        sleep 1
         echo `date +'%H:%M:%S: '` $f has changed
-        touch /etc/uwsgi/apps-available/leifdev.leiflundgren.com.ini
+        r=0
     done
+    return $r
+}
+
+while true; do
     sleep 1
+    if has_changed; then
+        echo 'touching uwsgi'
+        touch /etc/uwsgi/apps-available/leifdev.leiflundgren.com.ini
+    fi
 done
