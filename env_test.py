c30dc01c32deb2bb52fc63312a127ee0aa4919a3
Leif Lundgren <leiflundgren@leiflundgren.com>
2016-05-17 21:50:16 +0000

    working on flask-quest-string

--- env_test.py
+++ env_test.py
@@ -1,5 +1,5 @@
 from app_base import AppBase
-from flask import request
+import flask
 
 class EnvTest(AppBase):
     def __init__(self):
@@ -7,10 +7,16 @@ class EnvTest(AppBase):
         
     def application(self):
         # Sorting and stringifying the environment key, value pairs
-       response_body = request.query_string.decode(encoding='UTF-8')
+        self.log(5, "args-type: ", type(flask.request.args))
+        
+        response_body = ''
+        self.log(5, "response : ", str(response_body))
+        for (d, k, v) in flask.request.args:
+            self.log(5, "d-type= ", type(d), '  k-type=', type(k), '  v-type=', type(v))
+            response_body += str(k) + ": " + str(v)
 
-       self.log(5, "response-type: ", type(response_body))
-       self.log(5, "Environment:\n" + response_body)
+        self.log(5, "Environment:\n" + response_body)
        
-       return self.make_response(200, response_body, 'text/plain')
+        return self.make_response(200, response_body, 'text/plain')
+
 
