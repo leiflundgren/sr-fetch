706d21b6d2c96889d39692a5a9339718172327ac
Leif Lundgren <leiflundgren@leiflundgren.com>
2016-12-14 21:46:56 +0100

    added log

--- app_factory.py
+++ app_factory.py
@@ -56,6 +56,7 @@ if __name__ == '__main__':
     if debug:       
         # Bind to PORT if defined, otherwise default to 5000.
         port = int(os.environ.get('PORT', 5000))
+        print('Starting own http server on port ' + str(port))
         app.run(host='0.0.0.0', port=port, debug=debug)
 
     else:
